b0cca0160c2533e4671c0092c09497bb15f4eec0
Aidan <farinaaidan2@gmail.com>
2023-06-19 21:53:40 -0400

    created comments for generate logo function, created the part that will make the svg, and updated index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// this will bring over the generate logo function from shapes.js
+const { generateLogo } = require('./lib/shapes');
+
+// this will run the generate logo function in the shapes.js folder
+generateLogo();
